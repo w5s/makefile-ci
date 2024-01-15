819dc053e4699f5e5406361f3988ec95b6602f69
Julien Polo <julien.polo@captive.fr>
2024-01-15 17:44:04 +0100

    🐛 Fix and simplify ruby dependency installation

--- src/ruby.mk
+++ src/ruby.mk
@@ -25,46 +25,38 @@ ${BUNDLE_CACHE_PATH}:
 ${BUNDLE_PATH}: ${BUNDLE_CACHE_PATH}
 	@[ ! -z "${BUNDLE_PATH}" ] && ${MKDIRP} "${BUNDLE_PATH}"
 
-# Run bundle install only if yarn.lock or package-lock.json has changed
-$(MAKE_CACHE_PATH)/bundle-install: $(MAKE_CACHE_PATH) ${BUNDLE_CACHE_PATH} ${BUNDLE_PATH} $(MAKEFILE_LIST) $(wildcard Gemfile*)
+_bundle-install-required:
+	@bundle check
+
+# Add `bundle install` to `make install`
+PHONY += dependencies__ruby
+dependencies__ruby:
+	$(info Install Ruby dependencies...)
 	@if [ -z "$(BUNDLE_PATH)" ]; then \
 		${BUNDLE} config unset --local path; \
 	else \
 		${BUNDLE} config set --local path $(BUNDLE_PATH); \
 	fi
 	@${BUNDLE_INSTALL}
-	@${TOUCH} $@
-
-# Shortcut to bundle install
-PHONY += bundle-install-cached
-bundle-install-cached: $(MAKE_CACHE_PATH)/bundle-install
-	@:
-
-# Add `bundle install` to `make install`
-PHONY += dependencies__ruby
-dependencies__ruby:
-	$(info Install Ruby dependencies...)
-# 	@${RM} $(MAKE_CACHE_PATH)/bundle-install
-	@${MAKE} bundle-install-cached
 
 # Rubocop targets
 ifneq ($(RUBOCOP_ENABLED),)
 # Add rubocop to `make lint`
 PHONY += lint__rubocop
-lint__rubocop: bundle-install-cached
+lint__rubocop: _bundle-install-required
 	$(info Lint Ruby sources...)
 	@${RUBOCOP}
 
 # Add rubocop to `make format`
 PHONY += format__rubocop
-format__rubocop: bundle-install-cached
+format__rubocop: _bundle-install-required
 	$(info Format Ruby sources...)
 	@${RUBOCOP} -a
 endif
 
 # Add rspec to `make test`
 PHONY += test__rspec
-test__rspec: bundle-install-cached
+test__rspec: _bundle-install-required
 	$(info Test Ruby sources...)
 	@${RAKE} spec
 
