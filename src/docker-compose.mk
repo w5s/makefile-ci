31828949fbec71e662a2a02a44efaf053b99a9af
Julien Polo <julien.polo@captive.fr>
2024-01-18 11:38:58 +0100

    🐛 Fix docker-compose- targets

--- src/docker-compose.mk
+++ src/docker-compose.mk
@@ -3,17 +3,20 @@
 COMPOSE := docker compose
 COMPOSE_FILE := compose-dev.yaml
 
-PHONY += docker-compose-exec
-docker-compose-exec: export DOCKER_BUILD_TARGET ?= $(CONTAINER_CI_TARGET)
-docker-compose-exec: export APP_IMAGE = $(CONTAINER_CI_IMAGE):$(CONTAINER_CI_TAG)
-docker-compose-exec: export COMPOSE_PROJECT_NAME ?= $(CI_PROJECT_NAME)-$(shell date '+%Y%m%d%H%M%S')
-docker-compose-exec:
-	@trap "echo '[Docker] Stopping...';$(COMPOSE) -f $(COMPOSE_FILE) down --volumes --remove-orphans &>/dev/null" EXIT ERR; \
-	echo '[Docker] Starting...'; \
-	$(COMPOSE) -f $(COMPOSE_FILE) up --detach; \
-	echo '[Docker] Executing command...'; \
-	sleep 10;$(COMPOSE) -f $(COMPOSE_FILE) exec web /bin/bash -c "echo 'ICI'"
+COMPOSE_MAIN_SERVICE ?= web
+
+PHONY += docker-compose-run
+docker-compose-run: export DOCKER_BUILD_TARGET ?= $(CONTAINER_CI_TARGET)
+docker-compose-run: export APP_IMAGE = $(CONTAINER_CI_IMAGE):$(CONTAINER_CI_TAG)
+docker-compose-run: export COMPOSE_PROJECT_NAME ?= $(CI_PROJECT_NAME)-$(shell date '+%Y%m%d%H%M%S')
+docker-compose-run:
+# $(COMPOSE) -f $(COMPOSE_FILE) exec web /bin/bash -c "$(DOCKER_COMMAND)"
+	@trap "echo '[Docker Compose] Stopping...';$(COMPOSE) -f $(COMPOSE_FILE) down --volumes --remove-orphans &>/dev/null" EXIT; \
+	echo '[Docker Compose] Starting...'; \
+	$(COMPOSE) -f $(COMPOSE_FILE) up --detach --quiet-pull --exclude-services $(COMPOSE_MAIN_SERVICE); \
+	echo '[Docker Compose] Executing command...'; \
+	$(COMPOSE) -f $(COMPOSE_FILE) run $(COMPOSE_MAIN_SERVICE) $(DOCKER_COMMAND)
 
 PHONY += docker-compose-make-%
 docker-compose-make-%:
-	@$(MAKE) docker-compose-exec DOCKER_COMMAND="make $*"
+	@$(MAKE) docker-compose-run DOCKER_COMMAND="make $*"
