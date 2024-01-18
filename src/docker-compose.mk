2a55695a8160612a8c2d6a3898c7505e65d77cbe
Julien Polo <julien.polo@captive.fr>
2024-01-18 21:38:49 +0100

    🐛 Fix misc docker compose glitches

--- src/docker-compose.mk
+++ src/docker-compose.mk
@@ -10,12 +10,12 @@ docker-compose-run: export DOCKER_BUILD_TARGET ?= $(CONTAINER_CI_TARGET)
 docker-compose-run: export APP_IMAGE = $(CONTAINER_CI_IMAGE):$(CONTAINER_CI_TAG)
 docker-compose-run: export COMPOSE_PROJECT_NAME ?= $(CI_PROJECT_NAME)-$(shell date '+%Y%m%d%H%M%S')
 docker-compose-run:
-# $(COMPOSE) -f $(COMPOSE_FILE) exec web /bin/bash -c "$(DOCKER_COMMAND)"
-	@trap "echo '[Docker Compose] Stopping...';$(COMPOSE) -f $(COMPOSE_FILE) down --volumes --remove-orphans &>/dev/null" EXIT; \
+	@trap "echo '[Docker Compose] Stopping...';$(COMPOSE) -f $(COMPOSE_FILE) down --volumes --remove-orphans &>/dev/null" EXIT INT QUIT TERM; \
 	echo '[Docker Compose] Starting...'; \
-	$(COMPOSE) -f $(COMPOSE_FILE) up --detach --quiet-pull --wait; \
+	$(COMPOSE) -f $(COMPOSE_FILE) up --detach --remove-orphans --quiet-pull; \
 	echo '[Docker Compose] Executing command...'; \
-	$(COMPOSE) -f $(COMPOSE_FILE) run $(COMPOSE_MAIN_SERVICE) $(DOCKER_COMMAND)
+	$(COMPOSE) -f $(COMPOSE_FILE) run --use-aliases --rm $(COMPOSE_MAIN_SERVICE) $(DOCKER_COMMAND)
+
 
 PHONY += docker-compose-make-%
 docker-compose-make-%:
