e22dd9e32fdf530cee0c69d6bf7a094597c669bc
Julien Polo <julien.polo@captive.fr>
2024-01-26 11:40:34 +0100

    ♻️ Improve workflow
    Reason : be able to plug docker when in CI mode

--- src/workflow.mk
+++ src/workflow.mk
@@ -1,8 +1,14 @@
 .PHONY: all
 all: prepare install lint ## Run all targets
 
-.PHONY: prepare .prepare.pre .prepare .prepare.post
-prepare: .prepare.pre .prepare .prepare.post ## Install external dependencies
+
+#-------------
+# PREPARE
+#-------------
+.PHONY: prepare prepare.ci prepare.default .prepare.pre .prepare .prepare.post
+prepare: .workflow-run-prepare ## Install external dependencies
+prepare.default: .prepare.pre .prepare .prepare.post
+prepare.ci: prepare.default # TODO: implement this
 .prepare.pre::
 	@:
 .prepare::
@@ -10,8 +16,13 @@ prepare: .prepare.pre .prepare .prepare.post ## Install external dependencies
 .prepare.post::
 	@:
 
-.PHONY: dependencies .dependencies.pre .dependencies .dependencies.post
-dependencies: .dependencies.pre .dependencies .dependencies.post ## Install all dependencies
+#-------------
+# DEPENDENCIES
+#-------------
+.PHONY: dependencies dependencies.ci dependencies.default .dependencies.pre .dependencies .dependencies.post
+dependencies: .workflow-run-dependencies ## Install all dependencies
+dependencies.default: .dependencies.pre .dependencies .dependencies.post
+dependencies.ci: dependencies.default # TODO: implement this
 .dependencies.pre::
 	@:
 .dependencies::
@@ -19,8 +30,13 @@ dependencies: .dependencies.pre .dependencies .dependencies.post ## Install all 
 .dependencies.post::
 	@:
 
-.PHONY: build .build.pre .build .build.post
-build: .build.pre .build .build.post ## Build sources
+#-------------
+# BUILD
+#-------------
+.PHONY: build build.ci build.default .build.pre .build .build.post
+build: .workflow-run-build ## Build sources
+build.default: .build.pre .build .build.post
+build.ci: .build.default
 .build.pre::
 	@:
 .build::
@@ -28,8 +44,13 @@ build: .build.pre .build .build.post ## Build sources
 .build.post::
 	@:
 
-.PHONY: clean .clean.pre .clean .clean.post
-clean: .clean.pre .clean .clean.post ## Clean build files
+#-------------
+# CLEAN
+#-------------
+.PHONY: clean clean.ci clean.default .clean.pre .clean .clean.post
+clean: .workflow-run-clean ## Clean build files
+clean.default: .clean.pre .clean .clean.post
+clean.ci: .clean.default # TODO: implement this
 .clean.pre::
 	@:
 .clean::
@@ -37,8 +58,13 @@ clean: .clean.pre .clean .clean.post ## Clean build files
 .clean.post::
 	@:
 
-.PHONY: lint .lint.pre .lint .lint.post
-lint: .lint.pre .lint .lint.post ## Lint all source files
+#-------------
+# LINT
+#-------------
+.PHONY: lint lint.ci lint.default .lint.pre .lint .lint.post
+lint: .workflow-run-lint ## Lint all source files
+lint.default: .lint.pre .lint .lint.post
+lint.ci: lint.default # TODO: implement this
 .lint.pre::
 	@:
 .lint::
@@ -46,8 +72,13 @@ lint: .lint.pre .lint .lint.post ## Lint all source files
 .lint.post::
 	@:
 
-.PHONY: format .format.pre .format .format.post
-format: .format.pre .format .format.post ## Format all source files
+#-------------
+# FORMAT
+#-------------
+.PHONY: format format.ci format.default .format.pre .format .format.post
+format: .workflow-run-format ## Format all source files
+format.default: .format.pre .format .format.post
+format.ci: format.default # TODO: implement this
 .format.pre::
 	@:
 .format::
@@ -55,8 +86,13 @@ format: .format.pre .format .format.post ## Format all source files
 .format.post::
 	@:
 
-.PHONY: test .test.pre .test .test.post
-test: .test.pre .test .test.post ## Run unit tests
+#-------------
+# TEST
+#-------------
+.PHONY: test test.ci test.default .test.pre .test .test.post
+test: .workflow-run-test ## Run unit tests
+test.default: .test.pre .test .test.post
+test.ci: test.default # TODO: implement this
 .test.pre::
 	@:
 .test::
@@ -64,11 +100,28 @@ test: .test.pre .test .test.post ## Run unit tests
 .test.post::
 	@:
 
-.PHONY: develop .develop.pre .develop .develop.post
-develop: .develop.pre .develop .develop.post ## Setups a local development environment
+#-------------
+# DEVELOP
+#-------------
+.PHONY: develop develop.ci develop.default .develop.pre .develop .develop.post
+develop: .workflow-run-develop ## Setups a local development environment
+develop.default: .develop.pre .develop .develop.post
+develop.ci: develop.default
 .develop.pre::
 	@:
 .develop::
 	@:
 .develop.post::
 	@:
+
+# This job will run
+.PHONY: .workflow-run-%
+ifneq ($(CI),)
+.workflow-run-%:
+	$(info [Make] Running in CI mode)
+	@${MAKE} $*.ci
+else
+.workflow-run-%:
+	$(info [Make] Running in local mode)
+	@${MAKE} $*.default
+endif
