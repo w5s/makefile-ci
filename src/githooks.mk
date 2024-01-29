9b12fd530f26bc8f5d36e3205ca9af794f7cadcb
Julien Polo <julien.polo@captive.fr>
2024-01-29 12:42:39 +0100

    🔊 Improve git logs

--- src/githooks.mk
+++ src/githooks.mk
@@ -6,7 +6,7 @@ ${PROJECT_GITHOOKS_PATH}/.keep:
 .PHONY: githooks-install
 githooks-install: ${PROJECT_GITHOOKS_PATH}/.keep
 ifneq ($(shell ${GIT} config core.hooksPath), $(PROJECT_GITHOOKS_PATH))
-	$(info Install Git hooks...)
+	$(call log,info,"[Git] Install hooks...",1)
 	@${GIT} config core.hooksPath ${PROJECT_GITHOOKS_PATH}
 endif
 
