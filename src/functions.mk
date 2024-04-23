7b7e3938f8fa43034d22c3d2f6e84b60e8ee910d
Julien Polo <julien.polo@captive.fr>
2024-04-23 15:27:55 +0200

    ♻️ Add functions module

---
+++ src/functions.mk
@@ -0,0 +1,15 @@
+# Display a password with **** + last 4 characters
+#
+# @example
+#  echo $(call mask-password,$(PASSWORD))
+#
+define mask-password
+$(shell [ -z '$(1)' ] && (echo '') || (echo "****$(shell echo $(1) | tail -c 4)"))
+endef
+
+# Escape a shell string passed as a single quoted string
+#
+# @example
+#  embeddable-text = $(call escape-shell,$(SOME_TEXT))
+#
+escape-shell = $(subst $(newline),\$(newline),$(subst ','\'',$(1)))
