ccc7dc6f22a2a46be8b95755390a16cf559290c8
Julien Polo <julien.polo@captive.fr>
2024-05-16 11:20:53 +0200

    ♻️ Handle quiet command in ruby

--- src/ruby.mk
+++ src/ruby.mk
@@ -27,18 +27,18 @@ ${BUNDLE_PATH}: ${BUNDLE_CACHE_PATH}
 
 _bundle-install-required:
 	@$(call log,info,"[Ruby] Ensure dependencies....",1)
-	@bundle check
+	$(Q)bundle check
 
 # Add `bundle install` to `make install`
 .PHONY: ruby-install
 ruby-install:
 	@$(call log,info,"[Ruby] Install dependencies....",1)
-	@if [ -z "$(BUNDLE_PATH)" ]; then \
+	$(Q)if [ -z "$(BUNDLE_PATH)" ]; then \
 		${BUNDLE} config unset --local path; \
 	else \
 		${BUNDLE} config set --local path $(BUNDLE_PATH); \
 	fi
-	@${BUNDLE_INSTALL}
+	$(Q)${BUNDLE_INSTALL}
 .dependencies:: ruby-install # Add `bundle install` to `make install`
 
 # Rubocop targets
@@ -47,13 +47,13 @@ ifneq ($(RUBOCOP_ENABLED),)
 .PHONY: ruby-lint
 ruby-lint: _bundle-install-required
 	@$(call log,info,"[Ruby] Lint sources...",1)
-	@${RUBOCOP}
+	$(Q)${RUBOCOP}
 .lint:: ruby-lint # Add rubocop to `make lint`
 
 .PHONY: ruby-format
 ruby-format: _bundle-install-required
 	@$(call log,info,"[Ruby] Format sources...",1)
-	@${RUBOCOP} -a
+	$(Q)${RUBOCOP} -a
 .format:: ruby-format # Add rubocop to `make format`
 
 endif
@@ -61,8 +61,8 @@ endif
 .PHONY: ruby-test
 ruby-test: _bundle-install-required
 	@$(call log,info,"[Ruby] Test sources...",1)
-	@${RAKE} db:migrate || echo "Warning: Migration failed"
-	@${RAKE} spec
+	$(Q)${RAKE} db:migrate || echo "Warning: Migration failed"
+	$(Q)${RAKE} spec
 .test:: ruby-test # Add rspec to `make test`
 
 endif
