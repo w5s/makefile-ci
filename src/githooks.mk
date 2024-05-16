a298d4502e4e23adbf61c4b846c2255267cf4df4
Julien Polo <julien.polo@captive.fr>
2024-05-16 11:19:53 +0200

    ♻️ Handle quiet command in githooks

--- src/githooks.mk
+++ src/githooks.mk
@@ -1,13 +1,13 @@
 # Create githooks path
 ${PROJECT_GITHOOKS_PATH}/.keep:
-	@${MKDIRP} ${PROJECT_GITHOOKS_PATH}
-	@${TOUCH} ${PROJECT_GITHOOKS_PATH}/.keep
+	$(Q)${MKDIRP} ${PROJECT_GITHOOKS_PATH}
+	$(Q)${TOUCH} ${PROJECT_GITHOOKS_PATH}/.keep
 
 .PHONY: githooks-install
 githooks-install: ${PROJECT_GITHOOKS_PATH}/.keep
 ifneq ($(shell ${GIT} config core.hooksPath), $(PROJECT_GITHOOKS_PATH))
 	@$(call log,info,"[Git] Install hooks...",1)
-	@${GIT} config core.hooksPath ${PROJECT_GITHOOKS_PATH}
+	$(Q)${GIT} config core.hooksPath ${PROJECT_GITHOOKS_PATH}
 endif
 
 .prepare:: githooks-install # Install githooks during `make prepare`
