8099362d32b6737cf0ec4e1d2c5ce21f8fd0775e
Julien Polo <julien.polo@captive.fr>
2024-05-31 12:13:35 +0200

    💬 Add texts

--- src/scalingo.mk
+++ src/scalingo.mk
@@ -5,11 +5,11 @@ SCALINGO_ARCHIVE_FILE := $(SCALINGO_CACHE_PATH)/scalingo-app.tar.gz
 ## Scalingo region (default: osc-fr1)
 SCALINGO_REGION ?= osc-fr1
 ## Scalingo app name (default: $(CI_PROJECT_NAME)-$(CI_ENVIRONMENT_NAME))
-SCALINGO_APP_NAME ?= $(CI_PROJECT_NAME)-$(CI_ENVIRONMENT_NAME)
+SCALINGO_APP ?= $(CI_PROJECT_NAME)-$(CI_ENVIRONMENT_NAME)
 
 .PHONY: scalingo-archive
 scalingo-archive:
-	@$(call log,info,"[Scalingo] Bundle $(SCALINGO_APP_NAME)...",1)
+	@$(call log,info,"[Scalingo] Bundle $(SCALINGO_APP)...",1)
 	$(Q)$(MKDIRP) $(dir $(SCALINGO_ARCHIVE_FILE))
 	$(Q)$(GIT) archive --prefix=master/ HEAD | gzip > ${SCALINGO_ARCHIVE_FILE}
 
@@ -22,6 +22,8 @@ scalingo-clean:
 
 .PHONY: scalingo-deploy
 scalingo-deploy: scalingo-archive
-	@$(call log,info,"[Scalingo] Deploy $(SCALINGO_APP_NAME)...",1)
-	$(Q)$(SCALINGO) --app $(SCALINGO_APP_NAME) --region=$(SCALINGO_REGION) deploy ${SCALINGO_ARCHIVE_FILE}
+	@$(call log,info,"[Scalingo] Deploy $(SCALINGO_APP)...",1)
+	@$(call log,info,"SCALINGO_APP=$(SCALINGO_APP)...",2)
+	@$(call log,info,"SCALINGO_REGION=$(SCALINGO_REGION)...",2)
+	$(Q)$(SCALINGO) --app $(SCALINGO_APP) --region=$(SCALINGO_REGION) deploy ${SCALINGO_ARCHIVE_FILE}
 .deploy::scalingo-deploy
