f0629a316088f7a9fb41f3f1b78ca54a16ecf4fe
Julien Polo <julien.polo@captive.fr>
2024-06-25 23:24:55 +0200

    ♻️ Ajoute le template compose-common.yaml

--- src/template.mk
+++ src/template.mk
@@ -5,3 +5,9 @@
 .circleci/config.yml: $(MAKEFILE_CI_TEMPLATE_PATH)/.circleci/config.yml.template
 	$(Q)$(MKDIRP) $(dir $@)
 	$(Q)cp -f $< $@
+
+.docker/compose-common.yaml: $(MAKEFILE_CI_TEMPLATE_PATH)/.docker/compose-common.yaml.template
+	$(Q)$(MKDIRP) $(dir $@)
+	$(Q)cp -f $< $@
+
+.docker: .docker/compose-common.yaml
