f0c2eceaf1c2f4fd48652cdbc8da354384fa2b04
Julien Polo <julien.polo@captive.fr>
2024-06-25 23:27:40 +0200

    ♻️ Prefix docker compose with DOCKER_COMPOSE_

--- src/docker-compose.mk
+++ src/docker-compose.mk
@@ -1,22 +1,29 @@
 
-# Docker Compose command
-COMPOSE := docker compose
-COMPOSE_FILE := compose-dev.yaml
+## Docker compose file
+DOCKER_COMPOSE_FILE ?=
+ifeq ($(DOCKER_COMPOSE_FILE),)
+	DOCKER_COMPOSE_FILE = $(firstword $(wildcard compose-dev.yml compose.yml docker-compose.yml))
+endif
 
-COMPOSE_MAIN_SERVICE ?= web
+# Docker Compose command
+DOCKER_COMPOSE := docker compose
+ifneq ($(DOCKER_COMPOSE_FILE),)
+	DOCKER_COMPOSE += -f $(DOCKER_COMPOSE_FILE)
+endif
+
+DOCKER_COMPOSE_MAIN_SERVICE ?= web
 
 .PHONY: docker-compose-run
 docker-compose-run: export DOCKER_BUILD_TARGET ?= $(CONTAINER_BUILDER_TARGET)
 docker-compose-run: export APP_IMAGE = $(CONTAINER_BUILDER_IMAGE):$(CONTAINER_BUILDER_TAG)
 docker-compose-run: export COMPOSE_PROJECT_NAME ?= $(CI_PROJECT_NAME)-$(shell date '+%Y%m%d%H%M%S')
 docker-compose-run:
-	@ cleanup() { $(call log,info,[Docker Compose] Stopping...,1);$(COMPOSE) -f $(COMPOSE_FILE) down --volumes --remove-orphans &>/dev/null; } \
+	@ cleanup() { $(call log,info,[Docker Compose] Stopping...,1);$(DOCKER_COMPOSE) down --volumes --remove-orphans &>/dev/null; } \
 	&& trap cleanup EXIT INT QUIT TERM \
 	&& $(call log,info,[Docker Compose] Starting...,1) \
-	&& $(COMPOSE) -f $(COMPOSE_FILE) up --detach --remove-orphans --quiet-pull \
+	&& $(DOCKER_COMPOSE) up --detach --remove-orphans --quiet-pull \
   && $(call log,info,[Docker Compose] Executing command...,1) \
-	&& $(COMPOSE) -f $(COMPOSE_FILE) run --use-aliases --rm $(DOCKER_RUN_ARGS) $(COMPOSE_MAIN_SERVICE) $(DOCKER_COMMAND);
-
+	&& $(DOCKER_COMPOSE) run --use-aliases --rm $(DOCKER_RUN_ARGS) $(DOCKER_COMPOSE_MAIN_SERVICE) $(DOCKER_COMMAND);
 
 .PHONY: docker-compose-make-%
 docker-compose-make-%:
