c63f72dae82f91ac0f8966a5e2cb1af21aa7b6b5
Julien Polo <julien.polo@captive.fr>
2025-01-16 18:33:26 +0100

    📝 Improve documentation

--- src/cocoapods.mk
+++ src/cocoapods.mk
@@ -4,11 +4,19 @@ POD_LOCKFILE ?= $(POD_FILE).lock
 POD_MANIFEST := $(dir $(POD_FILE))/Pods/Manifest.lock
 POD_INSTALL := cd $(dir $(POD_FILE)) && $(POD) install --repo-update
 
+#
+# Install pod executable using bundle
+#
 .PHONY: pod-setup
-pod-setup: ruby-dependencies
+pod-setup: $(wildcard ruby-dependencies)
 
+#
+# Force install Pod dependencies
+#
+# If present node dependencies will be installed before (useful for react-native, ionic, etc)
+#
 .PHONY: pod-install
-pod-install: pod-setup
+pod-install: pod-setup $(wildcard node-dependencies)
 	@$(call log,info,"[Pod] Install dependencies...",1)
 	$(Q)$(POD_INSTALL)
 .install:: pod-install	# Add `pod install` to `make install`
@@ -18,7 +26,9 @@ $(POD_MANIFEST): $(POD_LOCKFILE)
 	$(Q)$(POD_INSTALL)
 	$(Q)touch $(POD_MANIFEST)
 
-# Install dependencies only if needed
+#
+# Install Pod dependencies only if needed
+#
 .PHONY: pod-dependencies
 pod-dependencies: pod-setup $(POD_MANIFEST)
 .dependencies:: pod-dependencies
