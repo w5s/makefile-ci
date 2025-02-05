30f368e17698ca98afbbbdfab7e47ebf8ed88822
Julien Polo <julien.polo@captive.fr>
2025-02-05 19:04:55 +0100

    🐛 Fix MAKE_PIDFILE generation
    Reason : phony seems to break the behavior with only one trigger on targets that depends on $(MAKE_PIDFILE)

--- src/pid.mk
+++ src/pid.mk
@@ -7,14 +7,12 @@ MAKE_PIDFILE := $(MAKE_CACHE_PATH)/pid
 # my-target: $(MAKE_PIDFILE)
 #   <- This will be executed only on new command
 #
-.PHONY: $(MAKE_PIDFILE)
-$(MAKE_PIDFILE):
+$(MAKE_PIDFILE): FORCE
 # Write pid file only if changed.
 	$(Q)echo "$(MAKE_PPID)" | cmp -s - $@ || \
 		(echo "$(MAKE_PPID)" > $@ && \
 			$(call log,debug,[Make] MAKE_PID=$(MAKE_PPID) saved to "$@",0) \
 		)
-
 
 # Create or update make pid file before each job.
 # This will ensure that the file exists before each job
